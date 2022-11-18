42ddf822fcbef5d6e2db13eb79f8852025546c71
Abdullahi Ahmad <abunnazeer@gmail.com>
2022-11-18 12:34:34 -0800

    working on the voting functionality

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const candidateImage = document.querySelector('.cand__image');
 const candidatePosition = document.querySelector('.candidate__position');
 const voteYes = document.querySelector('.voting__yes');
 const voteNo = document.querySelector('.voting__no');
-
+const bottomMenu = document.querySelector('.menu');
 const candidateContainer = document.querySelector('.candidate__container');
+
+//voting variables
+const selectVote = document.querySelectorAll('input[name="president"]');
+const voteBtn = document.querySelector('.btn__style');
 //slide images and caption
 const slideDetails = [
   {
@@ -45,49 +49,49 @@ const slideDetails = [
 ];
 
 // candidate details
-const candidateDetails = [
-  [
-    {
-      image: 'assets/img/tinubu.png',
-      fullName: 'Ahmad Bola Tinubu',
-      position: 'APC Presidential Candidate',
-      noOfVote: 0,
-    },
-    {
-      image: 'assets/img/Atiku.png',
-      fullName: 'Atiku Abubakar',
-      position: 'PDP Presidential Candidate',
-      noOfVote: 0,
-    },
-    {
-      image: 'assets/img/Rabiu-Musa-Kwankwaso',
-      fullName: 'Rabiu Musa Kwankoso',
-      position: 'ANPP Presidential Candidate',
-      noOfVote: 0,
-    },
-  ],
+// const candidateDetails = [
+//   [
+//     {
+//       image: 'assets/img/tinubu.png',
+//       fullName: 'Ahmad Bola Tinubu',
+//       position: 'APC Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//     {
+//       image: 'assets/img/Atiku.png',
+//       fullName: 'Atiku Abubakar',
+//       position: 'PDP Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//     {
+//       image: 'assets/img/Rabiu-Musa-Kwankwaso',
+//       fullName: 'Rabiu Musa Kwankoso',
+//       position: 'ANPP Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//   ],
 
-  [
-    {
-      image: 'assets/img/Kashim-Shettima.png',
-      fullName: 'Kashim Shettima',
-      position: 'APC Vice Presidential Candidate',
-      noOfVote: 0,
-    },
-    {
-      image: 'assets/img/Ifeanyichukwu.png',
-      fullName: 'Ifeanyi Chukwu Arthur Okowa',
-      position: 'PDP Vice Presidential Candidate',
-      noOfVote: 0,
-    },
-    {
-      image: 'assets/img/Edwin-Ume-Ezeoke.png',
-      fullName: 'Chief Edwin Ume-Ezeoke',
-      position: 'ANPP Vice Presidential Candidate',
-      noOfVote: 0,
-    },
-  ],
-];
+//   [
+//     {
+//       image: 'assets/img/Kashim-Shettima.png',
+//       fullName: 'Kashim Shettima',
+//       position: 'APC Vice Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//     {
+//       image: 'assets/img/Ifeanyichukwu.png',
+//       fullName: 'Ifeanyi Chukwu Arthur Okowa',
+//       position: 'PDP Vice Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//     {
+//       image: 'assets/img/Edwin-Ume-Ezeoke.png',
+//       fullName: 'Chief Edwin Ume-Ezeoke',
+//       position: 'ANPP Vice Presidential Candidate',
+//       noOfVote: 0,
+//     },
+//   ],
+// ];
 
 const candidateDetail = [
   {
@@ -131,12 +135,14 @@ const candidateDetail = [
 // this navigate to vote page
 bannerVoteBtn?.addEventListener('click', () => (location.href = 'vote.html'));
 
-// Load the banner slides
+//Load the banner slides
 let i = 0;
 window.addEventListener('DOMContentLoaded', function () {
   setInterval(() => {
-    bannerImg.src = slideDetails[i].imgPth;
-    caption.textContent = slideDetails[i].caption;
+    if (bannerImg?.src && caption?.textContent) {
+      bannerImg.src = slideDetails[i].imgPth;
+      caption.textContent = slideDetails[i].caption;
+    }
     i++;
     if (i === slideDetails.length) {
       i = 0;
@@ -146,7 +152,10 @@ window.addEventListener('DOMContentLoaded', function () {
 
 //add candidate dynamically to the home page
 const displayCandidate = function (candidateDetails) {
-  candidateContainer.innerHTML = '';
+  if (candidateContainer?.innerHTML) {
+    candidateContainer.innerHTML = '';
+  }
+
   candidateDetails.forEach(function (cand, i) {
     const html = `<div class="candidate__list">
           <img src="${cand.image}" class="cand_image" alt="">
@@ -166,8 +175,42 @@ const displayCandidate = function (candidateDetails) {
           </div>
         </div>
         `;
-    // console.log(html);
-    candidateContainer.insertAdjacentHTML('afterbegin', html);
+
+    candidateContainer?.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayCandidate(candidateDetail);
+displayCandidate(candidateDetail.reverse());
+
+//bottom menu
+// bottomMenu.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   bottomMenu.classList.remove('menu__active');
+//   console.log(bottomMenu);
+// });
+
+//voting code
+let ix = [];
+const contingVote = document.querySelector('.countingvote');
+
+voteBtn?.addEventListener('click', function () {
+  selectVote.forEach(function (value, i) {
+    if (value.checked && i === 0) {
+      i++;
+      ix.push(i);
+      console.log(
+        //  `i have been selected ${(contingVote.textContent = ix.length)}`
+        `i have been selected ${i - 1} ${(candidateDetail[0].noOfVote =
+          ix.length)}`
+      );
+      // i++;
+    } else if (value.checked && i === i) {
+      i++;
+      ix.push(i);
+      console.log(
+        `i have been selected ${i - 1} ${(candidateDetail[1].noOfVote =
+          ix.length)}`
+      );
+    }
+  });
+  location.href = 'index.html';
+});
